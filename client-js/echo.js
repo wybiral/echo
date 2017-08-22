8f3434030cc391c517c45e3cbf20da6b60f280ea
davy <davy.wybiral@gmail.com>
2017-08-22 09:30:51 -0500

    Added hex encoding

--- client-js/echo.js
+++ client-js/echo.js
@@ -11,8 +11,21 @@ const encode = {
     },
     hex: {
         fromBuffer: buffer => {
+            const array = new Uint8Array(buffer);
+            const len = array.length;
+            let hex = '';
+            for (let i = 0; i < len; i++) {
+                hex += ('00' + array[i].toString(16)).slice(-2);
+            }
+            return hex;
         },
         toBuffer: hex => {
+            const len = hex.length / 2 | 0;
+            const array = new Uint8Array(len);
+            for (let i = 0; i < len; i++) {
+                array[i] = parseInt(hex.substr(i * 2, 2), 16);
+            }
+            return array.buffer;
         },
     },
     string: {
